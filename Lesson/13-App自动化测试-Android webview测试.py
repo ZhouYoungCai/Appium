ecf004d92cfc2f709dd9490edb8b7a26c8cb6f92
zhouyongcai <807923011@qq.com>
2023-12-04 17:21:36 +0800

    第1次修改

--- Lesson/13-App自动化测试-Android webview测试.py
+++
@@ -1,4 +0,0 @@
-1、环境准备
-    手机端：被测浏览器
-	PC端：安装chrome浏览器
-	客户端代码：desirecapability
--- Lesson/27-App自动化测试-appium源代码分析[选修].py
+++
@@ -1,9 +0,0 @@
-1、java1.8版本，配置环境变量
-2、Android SDK，配置环境变量
-3、appium desktop
-4、python 3
-5、appium python client 
-
-安装软件地址：
-https://ceshiren.com/t/topic/2270
-
